5d806741d440acdc2f753421bb220b56bbb85a3e
omerbaki <bakiomer@yahoo.com>
2014-12-12 23:55:27 +0200

    [Fix] Add NotificationsCreator

---
+++ YouWavesAPI/ForecastNotifications/ForecastAnlysisNotificationCreator/NotificationsCreator.cs
@@ -0,0 +1,40 @@
+﻿using ForecastAnalysisNotificationCreator.WaveForecastNotificationCreators;
+using ForecastAnalysisEntities;
+using ForecastNotificaitonEntities;
+using Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoggerFramework;
+
+namespace ForecastAnalysisNotificationCreator
+{
+    public interface INotificationsCreator
+    {
+        Task CreateNotifications();
+    }
+
+    public class NotificationsCreator : INotificationsCreator
+    {
+        private readonly IEnumerable<INotificationCreator> mNotificationCreators; 
+
+        public NotificationsCreator(
+            IEnumerable<INotificationCreator> aNotificationCreators)
+        {           
+            mNotificationCreators = aNotificationCreators;
+        }
+
+        public async Task CreateNotifications()
+        {
+            foreach (var notificationCreator in mNotificationCreators)
+            {
+                await notificationCreator.Create();
+            }              
+        }
+    }
+}
